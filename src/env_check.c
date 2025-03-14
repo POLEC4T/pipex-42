e3d2a2b1662cb70ae2e256d4ccb1f2089221b884
Milo Niemaz <mniemaz@z4r9p4.42lyon.fr>
2025-03-14 09:06:11 +0100

    secure strjoin

--- src/env_check.c
+++ src/env_check.c
@@ -6,7 +6,7 @@
 /*   By: mniemaz <mniemaz@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 11:43:43 by mniemaz           #+#    #+#             */
-/*   Updated: 2025/03/10 11:07:40 by mniemaz          ###   ########.fr       */
+/*   Updated: 2025/03/13 11:34:08 by mniemaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,21 @@ char	**get_paths(char **env)
 	return (NULL);
 }
 
+static char	*safe_ft_strjoin(t_data *d, char *s1, char *s2, char *to_free)
+{
+	char		*res;
+
+	res = ft_strjoin(s1, s2);
+	if (!res)
+	{
+		if (to_free)
+			free(to_free);
+		msg("ft_strjoin", ": ", strerror(errno), STDERR_FILENO);
+		exit_process(EXIT_FAILURE, d);
+	}
+	return (res);
+}
+
 char	*get_cmd_path(t_data *d, char *cmd)
 {
 	char	*curr_path;
@@ -43,11 +58,11 @@ char	*get_cmd_path(t_data *d, char *cmd)
 
 	if (cmd && ft_strlen(cmd) > 0 && access(cmd, X_OK) != -1)
 		return (cmd);
-	cmd = ft_strjoin("/", cmd);
+	cmd = safe_ft_strjoin(d, "/", cmd, NULL);
 	i = -1;
 	while (d->paths[++i])
 	{
-		curr_path = ft_strjoin(d->paths[i], cmd);
+		curr_path = safe_ft_strjoin(d, d->paths[i], cmd, cmd);
 		if (access(curr_path, X_OK) != -1)
 		{
 			free(cmd);
